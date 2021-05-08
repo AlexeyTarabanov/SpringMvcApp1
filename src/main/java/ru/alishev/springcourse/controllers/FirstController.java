c9ecca0e8ba8ec5516433123cf2a0b259a2e3566
Alexey Tarabanov <tarabanov@bk.ru>
2021-05-09 01:25:16 +0300

    add 19 lesson

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  @Controller - данная аннотация помечает класс как контроллер
@@ -16,17 +19,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/first")
 public class FirstController {
 
-    // в данном методе мы будем приниать get-запросы
-    // метод будет возвращать страницу представления
+    /** в данном методе мы будем приниать get-запросы.
+     *  метод будет возвращать страницу представления.
+     *  представления будут лежать в отдельной папке first, которая относится к контроллеру */
+    // для того, чтобы принимать параметры из url в этом методе
+    // мы должны внедрить объект HttpServletRequest
+    // и в этом объекте будут содержаться все сведения о поступающем http запросе пользователя
     @GetMapping("/hello")
-    public String helloPage() {
-        // представления будут лежать в отдельной папке first,
-        // которая относится к контроллеру
+    public String helloPage(HttpServletRequest request) {
+        // из объекта request мы можем получить параметры запроса
+        // ** для примера скажем, что в url пользователь должен будет передавать 2 параметра (имя и фамилию)
+        // получаем данные параметры по ключу
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    // теперь применим аннотацию @RequestParam для того, чтобы сделать тоже самое
+    // здесь мы используем большой объект, который в себе содержит все сведения об http - запросе поступающем
+    // из него мы берем только параметры
+    // используем аннотацию @RequestParam
+    // ей мы передаем ключ "name" и значение по этому ключу name
+    // будет положено в параметр "name"
+    // тоже саимое делаем для фамилии
+    public String goodByePage(@RequestParam (value = "name", required = false) String name,
+                              @RequestParam (value = "surname", required = false) String surname) {
+
+        System.out.println("Goodbye, " + name + " " + surname + "!");
+
         return "first/goodbye";
     }
  }
