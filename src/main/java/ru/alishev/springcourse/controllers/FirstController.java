9ec955f9ab835f66d64e4c8416a2e6c09c67025c
Alexey Tarabanov <tarabanov@bk.ru>
2021-05-09 22:23:48 +0300

    add 20 lesson

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,49 +9,72 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- @Controller - данная аннотация помечает класс как контроллер
- (и будет отсканирована с помощью @ComponentScan)
-
- @RequestMapping - используется для мапинга (связывания) с URL
- для всего класса или для конкретного метода обработчика
+ * @Controller - данная аннотация помечает класс как контроллер
+ * (и будет отсканирована с помощью @ComponentScan)
+ * @RequestMapping - используется для мапинга (связывания) с URL
+ * для всего класса или для конкретного метода обработчика
  */
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
-    /** в данном методе мы будем приниать get-запросы.
-     *  метод будет возвращать страницу представления.
-     *  представления будут лежать в отдельной папке first, которая относится к контроллеру */
-    // для того, чтобы принимать параметры из url в этом методе
-    // мы должны внедрить объект HttpServletRequest
-    // и в этом объекте будут содержаться все сведения о поступающем http запросе пользователя
-    @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        // из объекта request мы можем получить параметры запроса
-        // ** для примера скажем, что в url пользователь должен будет передавать 2 параметра (имя и фамилию)
-        // получаем данные параметры по ключу
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    /**
+     * в данном методе мы будем приниать get-запросы.
+     * метод будет возвращать страницу представления.
+     * представления будут лежать в отдельной папке first, которая относится к контроллеру
+     */
 
-        System.out.println("Hello, " + name + " " + surname);
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        // c помощью метода кладем в модель пару ключ=значение
+        // здесь мы должны выбрать ключ, ключ - это строка и строка может быть любой
+        // в данном случаем выберм ключ "message"
+        // и здесь мы должны положить значение
+        // значение это строка: "Hello, " + name + " " + surname
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
+        // System.out.println("Hello, " + name + " " + surname);
 
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    // теперь применим аннотацию @RequestParam для того, чтобы сделать тоже самое
-    // здесь мы используем большой объект, который в себе содержит все сведения об http - запросе поступающем
-    // из него мы берем только параметры
-    // используем аннотацию @RequestParam
-    // ей мы передаем ключ "name" и значение по этому ключу name
-    // будет положено в параметр "name"
-    // тоже саимое делаем для фамилии
-    public String goodByePage(@RequestParam (value = "name", required = false) String name,
-                              @RequestParam (value = "surname", required = false) String surname) {
-
-        System.out.println("Goodbye, " + name + " " + surname + "!");
-
+    public String goodByePage() {
         return "first/goodbye";
     }
- }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam (value = "action") String action,
+                             Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b; // 1 / 2 = 0.5
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
+    }
+}
